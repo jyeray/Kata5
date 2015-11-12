ae760501aae0e52200cb738d839897fcaa15ae8a
joaquin <joaquin@joaquin-Notebook>
2015-11-12 09:52:44 +0000

    Version 2. Insertamos emails en la base de datos SQLite

--- src/kata5/KATA5.java
+++ src/kata5/KATA5.java
@@ -1,5 +1,10 @@
 package kata5;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -8,14 +13,24 @@ import java.sql.Statement;
 
 public class KATA5 {
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-//        Class.forName("org.sqlite.JDBC");
-//        Connection c = DriverManager.getConnection("jdbc:sqlite:KATADB");
-        Class.forName("oracle.jdbc.driver.OracleDriver");
-        Connection c = DriverManager.getConnection("jdbc:oracle:thin:@10.22.143.90:1521:orcl","system","orcl");
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, FileNotFoundException, IOException {
+        Class.forName("org.sqlite.JDBC");
+        Connection c = DriverManager.getConnection("jdbc:sqlite:KATADB");
+//        Class.forName("oracle.jdbc.driver.OracleDriver");
+//        Connection c = DriverManager.getConnection("jdbc:oracle:thin:@10.22.143.90:1521:orcl","system","orcl");
         System.out.println("Opened database successfully");
 
         Statement stmt = c.createStatement();
+        String fileName="/home/joaquin/NetBeansProjects/KATA5/DATA/emailsfilev1.txt";
+        BufferedReader reader = new BufferedReader(new FileReader(new File(fileName)));
+        
+        String mail;
+        while((mail=reader.readLine())!=null){
+            String query="INSERT INTO MAILS(MAIL) VALUES ('"+mail+"')";
+            stmt.executeUpdate(query);
+        }
+        
+        
         ResultSet rs = stmt.executeQuery("Select * from PEOPLE");
 
         while (rs.next()) {
